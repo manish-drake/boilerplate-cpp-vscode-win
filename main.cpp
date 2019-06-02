89d6000c31d3915b6ab250021b9e583a2de11fe2
manish-drake <manish@drake.in>
2019-06-02 21:33:42 +0530

    Added the main file

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+#define UNUSED(x) (void)(x)
+int main(int argc, char* argv[]){
+    UNUSED(argc);
+    UNUSED(argv);
+    
+    cin.ignore();
+    return 0;
+}
